1c5f298a9465958236f2a70c58f10a7e8beaf2e5
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-08-11 15:24:20 +0800

    feat: add seo

--- src/config.ts
+++ src/config.ts
@@ -6,10 +6,12 @@ const config = {
   description: `Welcome to ckt1031's cool website! Expect regular updates as I release exciting new content.
   I started this site in June 2022, so stay tuned for all the latest updates!`,
   author: 'cktsun', // Your name.
-  siteTitle: "ckt1031's blog", // Site title.
+  siteTitle: 'ckt1031', // Site title.
   siteIconPublicPath: '/favicon.ico', // Path to the public folder, where the site icon is located.
 
   github_project_url: 'https://github.com/ckt1031/ckt1031.xyz',
+
+  twitter: '@cktsun1031', // Twitter username.
 
   social: {
     discordServer: 'https://discord.gg/Y2ZnZc5Nnh',
