ea69189ebea86d1013a242ce20461701ba0bf459
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-08-31 10:45:16 +0000

    feat: upgrade to astro v3

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="../.astro/types.d.ts" />
-/// <reference types="astro/client-image" />
-/// <reference types="astro/client-image" />
+/// <reference types="astro/client" />
+/// <reference types="astro/client" />
 
 interface ImportMetaEnv {
   readonly SITE_URL: StringifyOptions;
