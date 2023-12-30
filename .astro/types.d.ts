ade0b97e7afa024a45c9d9bfcf58ceef60d672c6
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2023-12-30 18:19:23 +0800

    feat: update Astro dependencies and components

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -201,6 +201,13 @@ declare module 'astro:content' {
 
 	type ContentEntryMap = {
 		posts: {
+			'my-accomplishments-in-2023.md': {
+				id: 'my-accomplishments-in-2023.md';
+				slug: 'my-accomplishments-in-2023';
+				body: string;
+				collection: 'posts';
+				data: InferEntrySchema<'posts'>;
+			} & { render(): Render['.md'] };
 			'what-is-this-site.md': {
 				id: 'what-is-this-site.md';
 				slug: 'what-is-this-site';
