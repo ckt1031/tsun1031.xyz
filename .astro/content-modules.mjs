e7691069024256d9455b2c807fb58a892546a744
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-12-13 12:04:30 +0800

    feat: update content module paths and enhance contact page messaging

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,12 @@
 export default new Map([
 	[
+		'src/content/posts/my-accomplishments-in-2023.mdx',
+		() =>
+			import(
+				'astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmy-accomplishments-in-2023.mdx&astroContentModuleFlag=true'
+			),
+	],
+	[
 		'src/content/posts/my-hkdse-result.mdx',
 		() =>
 			import(
@@ -11,13 +18,6 @@ export default new Map([
 		() =>
 			import(
 				'astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fwhat-is-this-site.mdx&astroContentModuleFlag=true'
-			),
-	],
-	[
-		'src/content/posts/my-accomplishments-in-2023.mdx',
-		() =>
-			import(
-				'astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmy-accomplishments-in-2023.mdx&astroContentModuleFlag=true'
 			),
 	],
 ]);
