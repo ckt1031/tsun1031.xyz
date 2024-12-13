b2db4e29a883e581e3214112d476edc1a4d30002
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2024-12-13 12:06:35 +0800

    feat: swap content module paths and update dependencies

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -7,17 +7,17 @@ export default new Map([
 			),
 	],
 	[
-		'src/content/posts/my-hkdse-result.mdx',
-		() =>
-			import(
-				'astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmy-hkdse-result.mdx&astroContentModuleFlag=true'
-			),
-	],
-	[
 		'src/content/posts/what-is-this-site.mdx',
 		() =>
 			import(
 				'astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fwhat-is-this-site.mdx&astroContentModuleFlag=true'
+			),
+	],
+	[
+		'src/content/posts/my-hkdse-result.mdx',
+		() =>
+			import(
+				'astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fmy-hkdse-result.mdx&astroContentModuleFlag=true'
 			),
 	],
 ]);
