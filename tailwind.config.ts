c2e3933c65bde2a64600f7135a33798f2ed1e16f
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-01-17 11:45:25 +0800

    feat(package.json): update font packages to use lxgw-wenkai-tc and merriweather feat(main.astro): update font families to use lxgw-wenkai-tc and merriweather feat(tailwind.config.ts): update content paths to include only necessary files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'tailwindcss';
 import colors from 'tailwindcss/colors';
 
 const config: Config = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,md,mdx,ts,}'],
 	darkMode: ['class', '[data-theme="dark"]'],
 	plugins: [require('@tailwindcss/typography')],
 	theme: {
