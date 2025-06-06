0045c3c750cb112e92704f99b9d062d5b447d134
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-06-06 21:32:18 +0800

    refactor(env.d.ts): remove unused ImportMetaEnv interface and its reference

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,11 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
 /// <reference types="astro/client" />
-
-interface ImportMetaEnv {
-	readonly SITE_URL: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
