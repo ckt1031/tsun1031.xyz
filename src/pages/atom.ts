8eadf2d0dc411bce6bae2e6cbf5cfad7622166be
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2025-06-11 01:49:26 +0800

    refactor(atom.ts, rss.ts): streamline feed generation by moving logic to a dedicated function and enhancing post sorting

--- src/pages/atom.ts
+++ src/pages/atom.ts
@@ -1,32 +1,7 @@
-import { getCollection } from 'astro:content';
-import { Feed } from 'feed';
-
-import config from '@/config';
+import { generateFeed } from './rss';
 
 export async function GET(context: { site: string | URL }) {
-	const posts = await getCollection('posts');
-
-	const site = context.site;
-	const url = typeof site === 'string' ? site : site.href;
-
-	const feed = new Feed({
-		title: config.siteTitle,
-		description: config.description,
-		id: url,
-		link: url,
-		copyright: `All rights reserved ${new Date().getFullYear()}, ${config.siteTitle}`,
-	});
-
-	for (const post of posts) {
-		feed.addItem({
-			title: post.data.title,
-			id: `${url}posts/${post.slug}`,
-			link: `${url}posts/${post.slug}`,
-			description: post.data.description,
-			content: post.data.description,
-			date: post.data.published,
-		});
-	}
+	const feed = await generateFeed(context);
 
 	return new Response(feed.atom1(), {
 		headers: {
--- src/pages/rss.ts
+++ src/pages/rss.ts
@@ -1,26 +1,52 @@
-import { getCollection } from 'astro:content';
-import rss from '@astrojs/rss';
-
+import { loadRenderers } from 'astro:container';
+import { getCollection, render } from 'astro:content';
 import config from '@/config';
+import { getContainerRenderer as getMDXRenderer } from '@astrojs/mdx';
+import { experimental_AstroContainer as AstroContainer } from 'astro/container';
+import { Feed } from 'feed';
 
-export async function GET(context: { site: string | URL }) {
+export async function generateFeed(context: { site: string | URL }) {
+	const renderers = await loadRenderers([getMDXRenderer()]);
+	const container = await AstroContainer.create({ renderers });
 	const posts = await getCollection('posts');
 
-	return rss({
-		// `<title>` field in output xml
+	const site = context.site;
+	const url = typeof site === 'string' ? site : site.href;
+
+	const feed = new Feed({
 		title: config.siteTitle,
-		// `<description>` field in output xml
 		description: config.description,
-		// Pull in your project "site" from the endpoint context
-		// https://docs.astro.build/en/reference/api-reference/#contextsite
-		site: context.site,
-		// Array of `<item>`s in output xml
-		// See "Generating items" section for examples using content collections and glob imports
-		items: posts.map((post) => ({
+		id: url,
+		link: url,
+		copyright: `All rights reserved ${new Date().getFullYear()}, ${config.siteTitle}`,
+	});
+
+	for (const post of posts
+		.sort((a, b) => b.data.published.getTime() - a.data.published.getTime())
+		// Limit to 25 posts
+		.slice(0, 25)) {
+		const { Content } = await render(post);
+		const content = await container.renderToString(Content);
+
+		feed.addItem({
 			title: post.data.title,
-			pubDate: post.data.published,
+			id: `${url}posts/${post.slug}`,
+			link: `${url}posts/${post.slug}`,
 			description: post.data.description,
-			link: `/posts/${post.slug}/`,
-		})),
+			content,
+			date: post.data.published,
+		});
+	}
+
+	return feed;
+}
+
+export async function GET(context: { site: string | URL }) {
+	const feed = await generateFeed(context);
+
+	return new Response(feed.rss2(), {
+		headers: {
+			'Content-Type': 'application/xml; charset=utf-8',
+		},
 	});
 }
