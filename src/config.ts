8d2998eeca0b27f38f3b6c232611c35e63d3384e
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2026-02-08 22:10:27 +0800

    fix(config): include protocol in site URL to ensure valid absolute URLs

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 const config = {
-	url: 'tsun1031.xyz', // Site domain. Do not include a trailing slash!
+	url: 'https://tsun1031.xyz', // Site domain. Do not include a trailing slash!
 	description: `Welcome to ckt1031's cool website! Expect regular updates as I release exciting new content. I started this site in June 2022, so stay tuned for all the latest updates!`,
 	author: 'cktsun', // Your name.
 	siteTitle: 'ckt1031', // Site title.
