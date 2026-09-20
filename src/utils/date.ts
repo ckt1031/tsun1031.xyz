f7ef8aaa662069ca3fa7fe2ccd8dfc5d0eb1981b
ckt1031 <65409152+ckt1031@users.noreply.github.com>
2026-09-20 14:09:49 +0800

    fix(utils/date.ts): abbreviate month name from 'MMMM' to 'MMM' in Hong Kong date format to display shorter dates

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -43,9 +43,7 @@ export function parseHongKongDate(value: string) {
 }
 
 export function formatHongKongDate(date: Date | string) {
-	return dayjs(date)
-		.tz(HONG_KONG_TIME_ZONE)
-		.format('MMMM D, YYYY h:mm A [HKT]');
+	return dayjs(date).tz(HONG_KONG_TIME_ZONE).format('MMM D, YYYY h:mm A [HKT]');
 }
 
 export function formatHongKongShortDate(date: Date | string) {
